aa07fa2361846f32fad5fa10919bb1eeb7bdae1a
Thiago Neves <thiagomn03@gmail.com>
2023-11-19 13:42:30 -0300

    Implementado sort

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <p>Este é o frontend para o projeto da disciplina Arquitetura Java [23E4_2] do aluno Thiago Neves</p>
+      <p>Este frontend foi criado utilizando Vite e React 18.2 e node 18.16</p>
+      <p>O backend (api) foi criado em utilizando java 11 com springboot 2.7</p>
+      <p>A documentação da api pode ser acessada pelo link: <a href="http://localhost:8080/swagger-ui/index.html#/busca-controller" target="_blank" rel="noopener noreferrer">http://localhost:8080/swagger-ui/index.html#/busca-controller</a></p>
+      <br />
+      <p>O sort foi implementado no frontend e somente para vendedor.</p>
+    </>
   )
 }
--- src/views/vendedor.jsx
+++ src/views/vendedor.jsx
@@ -1,12 +1,20 @@
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faArrowDownZA, faArrowDown91, faArrowDown19, faArrowDownAZ } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react"
 import { Button, Table } from "reactstrap";
 import { useGlobalContext } from "../contexts/GlobalContext";
+import './vendedor.css'
 
 export default function Vendedor() {
   const [vendedores, setVendedores] = useState([]);
-  const {vendedorQuantidade, getVendedorQuantidade} = useGlobalContext()
+  const [orderedVendedores, setOrderedVendedores] = useState([])
+  const {getVendedorQuantidade} = useGlobalContext()
+  const [isAscending, setIsAscending] = useState(true)
+  const [isIdActive, setIsIdActive] = useState(true);
+  const [isNomeActive, setIsNomeActive] = useState(false);
+  const [isCpfActive, setIsCpfActive] = useState(false);
+  const [isEmailActive, setIsEmailActive] = useState(false);
+  const [isProdutosActive, setIsProdutosActive] = useState(false);
   
   const getVendedores = async () => {
     try {
@@ -40,30 +48,137 @@ export default function Vendedor() {
     }
   }
 
+  const sort = (field, isString) => {
+    let sortedVendedores = [...vendedores]
+    sortedVendedores.sort((a,b) => {
+      let auxA, auxB;
+      if(isString) {
+        auxA = a[field].toUpperCase();
+        auxB = b[field].toUpperCase();
+      } else {
+        auxA = a[field];
+        auxB = b[field];
+      }
+
+      let result = 0
+
+      if (auxA < auxB) {
+        result = -1;
+      }
+
+      if (auxA > auxB) {
+        result = 1;
+      }
+      return isAscending ? result : -result;
+    })
+
+    setOrderedVendedores([...sortedVendedores])
+  }
+
+  const handleId = () => {
+    sort('id', false);
+    setIsAscending(!isAscending)
+    setIsIdActive(true);
+    setIsNomeActive(false)
+    setIsCpfActive(false)
+    setIsEmailActive(false)
+    setIsProdutosActive(false)
+  }
+  const handleNome = () => {
+    sort('nome', true);
+    setIsAscending(!isAscending)
+    setIsIdActive(false);
+    setIsNomeActive(true)
+    setIsCpfActive(false)
+    setIsEmailActive(false)
+    setIsProdutosActive(false)
+  }
+  const handleCpf = () => {
+    sort('cpf', false);
+    setIsAscending(!isAscending)
+    setIsIdActive(false);
+    setIsNomeActive(false)
+    setIsCpfActive(true)
+    setIsEmailActive(false)
+    setIsProdutosActive(false)
+  }
+  const handleEmail = () => {
+    sort('email', true);
+    setIsAscending(!isAscending)
+    setIsIdActive(false);
+    setIsNomeActive(false)
+    setIsCpfActive(false)
+    setIsEmailActive(true)
+    setIsProdutosActive(false)
+  }
+  const handleProdutos = () => {
+    sort('produtos', false);
+    setIsAscending(!isAscending)
+    setIsIdActive(false);
+    setIsNomeActive(false)
+    setIsCpfActive(false)
+    setIsEmailActive(false)
+    setIsProdutosActive(true)
+  }
+
   useEffect(() => {
     getVendedores();
   }, [])
+
+  useEffect(() => {
+    if(vendedores.length) {
+      setOrderedVendedores(vendedores)
+    }
+  }, [vendedores])
 
   const handleDelete = async id => {
     await delVendedor(id)
     await getVendedores()
     await getVendedorQuantidade()
   }
+  function Thead({title, isAscending, string, isActive, handleClick}) {
+    const isString = !!string
+    function Icone() {
+      let iconComponent;
+      if (isString && isAscending) {
+        iconComponent = <FontAwesomeIcon icon={faArrowDownZA} />;
+      } else if (isString && !isAscending) {
+        iconComponent = <FontAwesomeIcon icon={faArrowDownAZ} />;
+      } else if (!isString && isAscending) {
+        iconComponent = <FontAwesomeIcon icon={faArrowDown91} />;
+      } else {
+        iconComponent = <FontAwesomeIcon icon={faArrowDown19} />;
+      }
+    
+      return (
+        <>
+          {iconComponent}
+        </>
+      );
+    }
+    return (
+      <th onClick={handleClick} className={`thead ${isActive ? 'active' : ''}`}>
+        <div className="thead-container">{title}
+        {isActive && <Icone/>}
+        </div>
+      </th>
+    )
+  }
   return (
     <main>
       <Table striped hover>
         <thead>
           <tr>
-            <th>#</th>
-            <th>Nome</th>
-            <th>CPF</th>
-            <th>email</th>
-            <th>produtos</th>
-            <th>ação</th>
+            <Thead isAscending={isAscending} isActive={isIdActive} handleClick={handleId} title="#" active/>
+            <Thead isAscending={isAscending} isActive={isNomeActive} handleClick={handleNome} title="Nome" string/>
+            <Thead isAscending={isAscending} isActive={isCpfActive} handleClick={handleCpf} title="CPF"/>
+            <Thead isAscending={isAscending} isActive={isEmailActive} handleClick={handleEmail} title="email" string/>
+            <Thead isAscending={isAscending} isActive={isProdutosActive} handleClick={handleProdutos} title="produtos"/>
+            <th >ação</th>
           </tr>
         </thead>
         <tbody>
-          {vendedores.map(vendedor => <tr key={`${vendedor.id}`}>
+          {orderedVendedores.map(vendedor => <tr key={`${vendedor.id}`}>
             <td>{vendedor.id}</td>
             <td>{vendedor.nome}</td>
             <td>{vendedor.cpf}</td>
